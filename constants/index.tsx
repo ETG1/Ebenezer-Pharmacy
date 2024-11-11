e86eaa83558742f5b7f063693e22bc69d3aa20f0
ETG1 <elijahkazungu22@gmail.com>
2024-11-11 05:05:58 +0200

    Nav bar & footer content

---
+++ constants/index.tsx
@@ -0,0 +1,84 @@
+import { link } from "fs";
+import { title } from "process";
+
+export const navBarLinks = [
+    {
+        title: "Home",
+        link: "/",
+    },
+    {
+        title: "Shop",
+        link: "/shop",
+    },
+    {
+        title: "Cart",
+        link: "/cart",
+    },
+];
+
+export const footerData = [
+    {
+        _id: 1,
+        title: "Get to know us",
+        listItem: [
+            {
+                _id: "001",
+                listData: [
+                    "Careers at Ebenezer",
+                    "Ebenzers' Blog",
+                    "About Ebenezer",
+                    "Ebenezer Devices",
+                    "Ebenezer Science"
+                ],
+            },
+        ],
+    },
+    {
+        _id: 2,
+        title: "Get to know us",
+        listItem: [
+            {
+                _id: "002",
+                listData: [
+                    "Careers at Ebenezer",
+                    "Ebenzers' Blog",
+                    "About Ebenezer",
+                    "Ebenezer Devices",
+                    "Ebenezer Science"
+                ],
+            },
+        ],
+    },
+    {
+        _id: 3,
+        title: "Get to know us",
+        listItem: [
+            {
+                _id: "003",
+                listData: [
+                    "Careers at Ebenezer",
+                    "Ebenzers' Blog",
+                    "About Ebenezer",
+                    "Ebenezer Devices",
+                    "Ebenezer Science"
+                ],
+            },
+        ],
+    },
+    {
+        _id: 4,
+        title: "Get to know us",
+        listItem: [
+            {
+                _id: "004",
+                listData: [
+                    "Careers at Ebenezer",
+                    "Ebenzers' Blog",
+                    "About Ebenezer",
+                    "Ebenezer Devices",
+                    "Ebenezer Science"
+                ],
+            },
+        ],
+    },
+]
