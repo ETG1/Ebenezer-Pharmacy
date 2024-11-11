2bf8d81630854580d1c88f22d696e8ad6a850814
ETG1 <elijahkazungu22@gmail.com>
2024-11-11 04:46:51 +0200

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Container from './Container'
+import Logo from './Logo';
+import SearchBar from './SearchBar';
+import { navBarLinks } from '@/constants';
+import Link from 'next/link';
+import { CgMenuRight } from "react-icons/cg";
+
+const Header = () => {
+  return (
+    <header className="w-full h-20 bg-accentWhite border-b-[1px] border-b-grayText/50 ">
+      <Container className="h-full flex items-center justify-between gap-5 lg:gap-10 ">
+        <Logo />
+        <SearchBar/>
+        <div className="hidden md:inline-flex items-center gap-7">
+          {navBarLinks?.map((item) => (
+            <Link key={item?.title} href={item?.link} className="navBar">
+              {item?.title}
+            </Link>
+          ))}
+          <Link href={"/signin"} className="navBar">
+            Sign in
+          </Link>
+          <Link href={"/studio"} className="navBar">
+            Studio
+          </Link>
+        </div>
+        <CgMenuRight className="inline-flex md:hidden cursor-pointer text-3xl navBar"/>
+      </Container>
+    </header>
+  )
+};
+
+export default Header
