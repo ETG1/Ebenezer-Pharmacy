aac4bc96d9aa2dbcd4eb008ac8954a1e4d58854e
ETG1 <elijahkazungu22@gmail.com>
2024-11-11 04:55:12 +0200

    Custom container

---
+++ components/Container.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface props {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Container = ({ children, className}: props) => {
+  return (
+    <div className={twMerge("max-w-screen-xl mx-auto px-2", className)}>
+      {children}
+    </div>
+  )
+}
+
+export default Container
