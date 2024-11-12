fc11175ddd3549e5854fb166ff674261bb265a5c
ETG1 <elijahkazungu22@gmail.com>
2024-11-12 14:37:14 +0200

    property Params now compatible

--- app/(user)/product/[slug]/page.tsx
+++ app/(user)/product/[slug]/page.tsx
@@ -8,9 +8,8 @@ import { urlFor } from '@/sanity/lib/image';
 import { ProductData } from '@/type';
 import { groq } from 'next-sanity';
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
 import { MdStar } from 'react-icons/md';
-
 
 interface Props {
   params: {
@@ -18,13 +17,12 @@ interface Props {
   };
 }
 
-const IndividualProductPage = async ({params}: Props) => {
-  const {slug} = await Promise.resolve(params);
-  const query = groq`*[_type == "product" && slug.current == $slug][0]{
-    ...
-  }`;
-  const product: ProductData[] = await client.fetch(query, {slug});
+const IndividualProductPage = async ({ params }: Props) => {
+  const { slug } = params; 
+  const query = groq`*[_type == "product" && slug.current == $slug][0]{ ... }`;
+  const product: ProductData = await client.fetch(query, { slug });
   const bestSellerData: ProductData[] = await getBestSellerData();
+
   return (
     <Container className="my-10 bg-grayText/20 rounded-xl">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-6 gap-4 h-full p-4">
@@ -37,9 +35,9 @@ const IndividualProductPage = async ({params}: Props) => {
             className="w-full h-full object-contain rounded-lg"
           /> 
         </div>
-        <div className="w-full md:col-span-2 xl:col-span-3 xl:p-14 flex flex-colgap-6 justify-center">
+        <div className="w-full md:col-span-2 xl:col-span-3 xl:p-14 flex flex-col gap-6 justify-center">
           <div className="flex flex-col gap-5 items-center">
-            <h2 className="text-4xl font-ssemibod">
+            <h2 className="text-4xl font-semibold">
               {product?.title}
             </h2>
             <div>
@@ -47,12 +45,12 @@ const IndividualProductPage = async ({params}: Props) => {
             </div>
             <div className="flex items-center gap-3">              
               <div className="text-base text-grayText flex items-center">
-                {Array?.from({length: 5})?.map((_,index) => {
-                  const filled = index + 1 <= Math.floor(product?.ratings);
-                  const halfFilled = index + 1 > Math.floor(product?.ratings) && index < Math.ceil(product?.ratings);
+                {Array.from({ length: 5 }).map((_, index) => {
+                  const filled = index + 1 <= Math.floor(product?.ratings || 0);
+                  const halfFilled = index + 1 > Math.floor(product?.ratings || 0) && index < Math.ceil(product?.ratings || 0);
                   return (
-                    <MdStar key={index} className={`${filled ? "text-[#fa8900]": halfFilled ? "text-yellow-200" : "text-grayText"}`}/>
-                  )
+                    <MdStar key={index} className={`${filled ? "text-[#fa8900]" : halfFilled ? "text-yellow-200" : "text-grayText"}`} />
+                  );
                 })}
               </div>
               <p className="text-sm font-semibold text-blackaccent/60 tracking-wide ">
@@ -62,17 +60,17 @@ const IndividualProductPage = async ({params}: Props) => {
             <p className="text-sm tracking-wide text-gray-600">
               {product?.description}
             </p>
-            <AddToCartButton item={product} className="rounded-full py-2"/>
+            <AddToCartButton item={product} className="rounded-full py-2" />
           </div>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-        {bestSellerData?.map((item) => (
-          <ProductCard key={item?._id} item={item}/>
+        {bestSellerData.map((item) => (
+          <ProductCard key={item?._id} item={item} />
         ))}
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default IndividualProductPage
+export default IndividualProductPage;
