d0bd1d2bb13b6f032a19821cdbdfebee4ef22ca8
ETG1 <elijahkazungu22@gmail.com>
2024-11-13 07:10:52 +0200

    shadcn CLI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        ceruleanBlue: "#268fcd",
-        limeGreen: "#79b84e",
-        grayText: "#888888",
-        blackaccent: "#000000",
-        accentWhite: "#FFFFFF",
-        brightRed: "#EF3636", // warning
-        neonGreen: "#41CE09", // success
-        bgLight: "#F5F5F5", // background
-      }, 
-    },
+  	extend: {
+  		colors: {
+  			ceruleanBlue: '#268fcd',
+  			limeGreen: '#79b84e',
+  			grayText: '#888888',
+  			blackaccent: '#000000',
+  			accentWhite: '#FFFFFF',
+  			brightRed: '#EF3636',
+  			neonGreen: '#41CE09',
+  			bgLight: '#F5F5F5'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 
 export default config;
